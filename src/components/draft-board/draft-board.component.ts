7cbdf26e5c3a4b83e7014cfb378413f1e329b9d3
tobiaskonzok <tobias.konzok@shinefour.de>
2024-08-10 08:07:16 +0200

    add search bar and clear button

--- src/components/draft-board/draft-board.component.ts
+++ src/components/draft-board/draft-board.component.ts
@@ -1,21 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import {JsonPipe, NgClass, NgForOf} from '@angular/common';
+import {JsonPipe, NgClass, NgForOf, NgIf} from '@angular/common';
 import { Player, PlayerStatus } from '../../domain/player';
 import { DraftBoardRowComponent } from './draft-board-row/draft-board-row.component';
 import { PlayerService } from '../../domain/player.service';
 import {Position} from "../position/position.component";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-draft-board',
   standalone: true,
-  imports: [NgForOf, JsonPipe, DraftBoardRowComponent, NgClass],
+  imports: [
+    NgForOf,
+    JsonPipe,
+    DraftBoardRowComponent,
+    NgClass,
+    FormsModule,
+    NgIf,
+  ],
   templateUrl: './draft-board.component.html',
   styleUrl: './draft-board.component.css',
 })
 export class DraftBoardComponent implements OnInit {
-  players: Player[] = [];
-  filteredPlayers: Player[] = [];
-  visiblePositions: Set<string> = new Set();
+  protected players: Player[] = [];
+  protected filteredPlayers: Player[] = [];
+  protected searchTerm: string = '';
+  protected readonly visiblePositions: Set<string> = new Set();
+  protected readonly Position = Position;
 
   constructor(private playerService: PlayerService) {}
 
@@ -24,11 +34,12 @@ export class DraftBoardComponent implements OnInit {
       this.players = players.filter(
         (player) => player.status === PlayerStatus.AVAILABLE,
       );
+      this.clearSearch();
       this.filterPlayers();
     });
   }
 
-  togglePosition(position: Position): void {
+  protected togglePosition(position: Position): void {
     if (this.visiblePositions.has(position)) {
       this.visiblePositions.delete(position);
     } else {
@@ -37,15 +48,30 @@ export class DraftBoardComponent implements OnInit {
     this.filterPlayers();
   }
 
-  private filterPlayers(): void {
-    if (this.visiblePositions.size === 0) {
-      this.filteredPlayers = this.players;
-    } else {
-      this.filteredPlayers = this.players.filter((player) =>
-        this.visiblePositions.has(player.pos),
-      );
-    }
+  protected filterPlayers(): void {
+    this.filteredPlayers = this.players.filter((player) => {
+      const matchesPosition =
+        this.visiblePositions.size === 0 ||
+        this.visiblePositions.has(player.pos);
+      return matchesPosition && this.matchesSearchTerm(player);
+    });
   }
 
-  protected readonly Position = Position;
+  protected showAll() {
+    this.visiblePositions.clear();
+    this.filterPlayers();
+  }
+
+  protected clearSearch() {
+    this.searchTerm = '';
+    this.filterPlayers();
+  }
+
+  private matchesSearchTerm(player: Player) {
+    let trimmedPlayerName = player.name.toLowerCase().replace(/[^a-zA-Z]/g, '');
+    let trimmedSearchTerm = this.searchTerm
+      .toLowerCase()
+      .replace(/[^a-zA-Z]/g, '');
+    return trimmedPlayerName.includes(trimmedSearchTerm);
+  }
 }
